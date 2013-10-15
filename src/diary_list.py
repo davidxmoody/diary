4445b307ef5f4c52142e3a09b31d801a02f5b189
David Moody <david@davidxmoody.com>
2013-10-15 14:47:34 +0100

    Combined diary_tui.py with diary_list.py

--- src/diary_list.py
+++ src/diary_list.py
@@ -1,9 +1,49 @@
-#!/usr/bin/env python3
-
 import diary_range
-from diary_tui import formatted
 import sys
 from subprocess import Popen, PIPE
+
+from config import pad_char, color_middle, color_padding, color_end, terminal_width
+import textwrap
+import time
+
+
+def get_date_string(entry, format='%A %d %B %Y %I:%M%p'):
+    '''Return formatted string representing the entry creation date.'''
+    return time.strftime(format, time.localtime(float(entry.timestamp)))
+
+def get_header(entry, width):
+    '''Return a colored header string padded to the correct width.'''
+
+    left = pad_char + '{} words'.format(entry.wordcount())
+    right = str(entry.timestamp) + pad_char
+    middle = ' {} '.format(get_date_string(entry))
+
+    padding_left = pad_char * int(width/2 - len(left) - len(middle)/2)
+    padding_right = pad_char * (width - len(left) - 
+                        len(padding_left) - len(middle) - len(right))
+
+    return color_padding +     left      + padding_left + color_end + \
+           color_middle  +            middle            + color_end + \
+           color_padding + padding_right +    right     + color_end
+
+def _gen_formatted(entry, width):
+    '''Return a generator over the formatted, wrapped lines of an entry.'''
+
+    yield get_header(entry, width)
+
+    wrapper = textwrap.TextWrapper(width=width)
+
+    for line in entry._gen_text():
+        wrapped_text = wrapper.wrap(line)
+        if len(wrapped_text) == 0:
+            yield ''
+        for wrapped_line in wrapped_text:
+            yield wrapped_line
+
+def formatted(entry, width=terminal_width):
+    '''Return a list of formatted lines, wrapped to width.'''
+    return '\n'.join(_gen_formatted(entry, width)) + '\n\n'
+
 
 def display_entries(entries):
     '''Open a less pipe to display the given entries to the user.'''
@@ -20,7 +60,6 @@ def display_entries(entries):
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        # TODO change default range to something better or use lazy evaluation
         display_entries(diary_range.last(100))
     else:
         display_entries(diary_range.process_args())
