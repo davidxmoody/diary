0d94db9df4ee8b87fe4b63e70f0154a54667028c
David Moody <david@davidxmoody.com>
2013-10-31 14:40:19 +0000

    Reformatted cli.py, adjusted TODOs

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,5 @@
-import argparse
-import diary_range
+from argparse import ArgumentParser
+from diary_range import connect
 from presenter import display_entries
 from fuzzydate import custom_date
 
@@ -10,8 +10,9 @@ VERSION_NUMBER = '2.0.0'
 #TODO add default base dir and device name
 
 
-# Setup main parser ###########################################################
-parser = argparse.ArgumentParser(description='') 
+# SETUP MAIN PARSER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+parser = ArgumentParser(description='') 
 parser.add_argument('--version', action='version', 
         version='{} {}'.format(PROGRAM_NAME, VERSION_NUMBER))
 parser.add_argument('-b', '--base', help='path to base folder')
@@ -20,9 +21,10 @@ subparsers = parser.add_subparsers()
 
 
 
-# 'edit' command ##############################################################
-# Edit the most recent entry or the entry specified by entry_id
+# EDIT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 def edit_command(conn, entry_id, **kwargs):
+    '''Edit the most recent entry or the entry specified by entry_id.'''
     entry = conn.find_by_id(entry_id) if entry_id else conn.most_recent_entry()
     entry.command_line_edit()
 
@@ -32,9 +34,10 @@ subparser.set_defaults(func=edit_command)
 
 
 
-# 'new' command ###############################################################
-# Create and edit a new entry with the current date or the given date
+# NEW ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 def new_command(conn, date, **kwargs):
+    '''Create and edit a new entry with the current date or the given date.'''
     entry = conn.new_entry(date)
     entry.command_line_edit()
 
@@ -44,9 +47,10 @@ subparser.set_defaults(func=new_command)
 
 
 
-# 'search' command ############################################################
-# Display all entries containing all of the given search_terms
+# SEARCH ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 def search_command(conn, search_terms, descending, after, before, **kwargs):
+    '''Display entries containing all of the terms in search_terms.'''
     entries = conn.search_entries(*search_terms, descending=descending, 
                                   min_date=after, max_date=before)
     display_entries(entries, search_terms)
@@ -55,19 +59,16 @@ subparser = subparsers.add_parser('search', aliases=['list'])
 subparser.add_argument('search_terms', nargs='*')
 subparser.add_argument('--before', type=custom_date, metavar='DATE')
 subparser.add_argument('--after', type=custom_date, metavar='DATE')
-#TODO add --on option
-#TODO try out --to and --from arguments instead?
 subparser.add_argument('--asc', action='store_false', dest='descending')
 subparser.add_argument('--desc', action='store_true', dest='descending')
-subparser.set_defaults(descending=True)
 subparser.set_defaults(func=search_command, descending=True)
 
 
 
-# 'wordcount' command #########################################################
-# Pretty print wordcount statistics for all entries
-def wordcount_command(conn, group_by, **kwargs):
+# WORDCOUNT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+def wordcount_command(conn, group_by, **kwargs):
+    '''Pretty print wordcount statistics for all entries.'''
     if group_by is None: group_by = 'Total'
     wordcounts = {}
     entry_counts = {}
@@ -79,14 +80,16 @@ def wordcount_command(conn, group_by, **kwargs):
         wordcounts[group] += entry.wordcount
         entry_counts[group] += 1
 
-    results = [ (group, wordcounts[group], entry_counts[group]) for group in sorted(wordcounts.keys()) ]
+    results = [ (group, wordcounts[group], entry_counts[group]) 
+                            for group in sorted(wordcounts.keys()) ]
 
     if len(results)>1:
-        results.append( ('Total', sum(wordcounts.values()), sum(entry_counts.values())) )
+        results.append( ('Total', sum(wordcounts.values()), 
+                                  sum(entry_counts.values())) )
     
-    max_lengths = { 'len_group': max( len(str(result[0])) for result in results ),
-                    'len_wc': len(str(results[-1][1])),
-                    'len_ec': len(str(results[-1][2])) }
+    max_lengths = {'len_group': max(len(str(result[0])) for result in results),
+                   'len_wc': len(str(results[-1][1])),
+                   'len_ec': len(str(results[-1][2]))}
 
     format_string = '{0:>{len_group}}:  {1:>{len_wc}} words,  {2:>{len_ec}} entries'
 
@@ -103,11 +106,12 @@ subparser.set_defaults(func=wordcount_command)
 
 
 
-# Process args ################################################################
+# PROCESS ARGS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 def process_args():
     args = parser.parse_args()
     if hasattr(args, 'func'):
-        conn = diary_range.connect(args.base)
+        conn = connect(args.base)
         args.func(conn, **vars(args))
     else:
         parser.print_usage()
