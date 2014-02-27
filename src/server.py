13a76ada474fa613f38e4b12c6fe3262b8bde981
David Moody <david@davidxmoody.com>
2014-02-27 15:54:35 +0000

    In progress implementation of server.py

--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask.ext.restful import Api, Resource
+from flask import Flask, abort
+from flask.ext.restful import Api, Resource, marshal_with, fields, marshal
 from diary_range import connect
 
 app = Flask(__name__)
@@ -7,28 +7,42 @@ api = Api(app)
 
 conn = connect('~/.diary')
 
-class Entries(Resource):
-    def get(self):
-        entries = list(conn.get_entries())
-        return [ {
-            'id': entry.id,
-            #'date': entry.date,
-            'wordcount': entry.wordcount,
-            'text': entry.text
-        } for entry in entries ]
+entry_fields = {
+    'id': fields.String,
+    'mtime': fields.Integer,
+    'date': fields.DateTime,
+    'text': fields.String,
+    'html': fields.String,
+    'wordcount': fields.Integer
+}
+
 
 class Entry(Resource):
+    @marshal_with(entry_fields)
     def get(self, entry_id):
+        #TODO add custom fields option
         entry = conn.find_by_id(entry_id)
-        return {
-            'id': entry.id,
-            #'date': entry.date,
-            'wordcount': entry.wordcount,
-            'text': entry.text
-        }
+        if entry is None: abort(404)
+        return entry
 
-api.add_resource(Entries, '/entries')
+    def put(self, entry_id):
+        pass
+
+    def delete(self, entry_id):
+        pass
+
+
+class Entries(Resource):
+    def get(self):
+        entries = conn.get_entries()
+        return [marshal(entry, entry_fields) for entry in entries]
+
+    def post(self):
+        pass
+
+
 api.add_resource(Entry, '/entries/<string:entry_id>')
+api.add_resource(Entries, '/entries')
 
 if __name__ == '__main__':
     app.run(debug=True)
