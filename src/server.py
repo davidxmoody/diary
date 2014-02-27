de66f43c73406239eeb6bd9ac536f2e4304f2081
David Moody <david@davidxmoody.com>
2014-02-27 17:13:23 +0000

    Added custom fields option

--- src/server.py
+++ src/server.py
@@ -9,7 +9,7 @@ api = Api(app)
 
 conn = connect('~/.diary')
 
-entry_fields = {
+default_entry_fields = {
     'id': fields.String,
     'mtime': fields.Integer,
     'date': fields.DateTime,
@@ -18,13 +18,24 @@ entry_fields = {
     'wordcount': fields.Integer
 }
 
+def marshal_entry(entry, fields_list=None):
+    if fields_list is None:
+        fields_dict = default_entry_fields
+    else:
+        fields_dict = { k: v for k, v in default_entry_fields.items() 
+                                                    if k in fields_list }
+    return marshal(entry, fields_dict)
+
+
+entry_get_parser = reqparse.RequestParser()
+entry_get_parser.add_argument('fields', type=str, action='append', choices=default_entry_fields.keys())
 
 class Entry(Resource):
     def get(self, entry_id):
-        #TODO add custom fields option
+        args = entry_get_parser.parse_args()
         entry = conn.find_by_id(entry_id)
         if entry is None: abort(404)
-        return marshal(entry, entry_fields)
+        return marshal_entry(entry, args['fields'])
 
     def put(self, entry_id):
         pass
@@ -33,21 +44,24 @@ class Entry(Resource):
         pass
 
 
-parser = reqparse.RequestParser()
-parser.add_argument('before', type=custom_date)
-parser.add_argument('after', type=custom_date)
-parser.add_argument('modifiedSince', type=int, default=0)
-parser.add_argument('order', type=str, default='asc')
-parser.add_argument('q', type=str, action='append', default=[])
-parser.add_argument('fields', type=str, action='append')
-parser.add_argument('offset', type=int, default=0)
-parser.add_argument('limit', type=int, default=50)
+#TODO move into the Entries class
+entries_get_parser = reqparse.RequestParser()
+entries_get_parser.add_argument('before', type=custom_date)
+entries_get_parser.add_argument('after', type=custom_date)
+entries_get_parser.add_argument('modifiedSince', type=int, default=0)
+entries_get_parser.add_argument('order', type=str, default='asc')
+entries_get_parser.add_argument('q', type=str, action='append', default=[])
+#TODO represent fields as a list of strings rather than multiple occurrences of strings
+# i.e. make it so that `?fields=date,id` works instead of having to do
+# `?fields=date&fields=id`
+entries_get_parser.add_argument('fields', type=str, action='append', choices=default_entry_fields.keys())
+entries_get_parser.add_argument('offset', type=int, default=0)
+entries_get_parser.add_argument('limit', type=int, default=50)
 
 
 class Entries(Resource):
     def get(self):
-        args = parser.parse_args()
-        print(args)
+        args = entries_get_parser.parse_args()
         entries = conn.search_entries(*args['q'], 
                                       descending=(args['order']=='desc'), 
                                       max_date=args['before'],
@@ -55,7 +69,7 @@ class Entries(Resource):
         entries = filter(lambda e: e.mtime>args['modifiedSince'], entries)
         entries = islice(entries, args['offset'], args['offset']+args['limit'])
 
-        return [marshal(entry, entry_fields) for entry in entries]
+        return [marshal_entry(entry, args['fields']) for entry in entries]
 
     def post(self):
         pass
