094aafba24b95e1aab837dca46d3d2ef7b2cde07
David Moody <david@davidxmoody.com>
2014-03-05 11:44:31 +0000

    Restructured directories, added first test case

---
+++ tests/test_fuzzydate.py
@@ -0,0 +1,9 @@
+import unittest
+from server.fuzzydate import custom_date
+
+class TestCustomDate(unittest.TestCase):
+    def test_custom_date(self):
+        date = custom_date('2012-02-03')
+        self.assertEqual(date.year, 2012)
+        self.assertEqual(date.month, 2)
+        self.assertEqual(date.day, 3)
