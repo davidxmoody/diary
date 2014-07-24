58d367b98c3a4f91800b4a8528521a1d7a48083a
David Moody <david@davidxmoody.com>
2014-07-24 17:15:29 +0100

    In-progress implementation of better CSS

--- diary/generator.py
+++ diary/generator.py
@@ -30,14 +30,31 @@ def generate_command(conn, out, watch, clean, **kwargs):
         if day not in days:
             days[day] = []
         days[day].append(entry)
+
+    # Calculate which is the next and previous days relative to each day
+    previous_days = {}
+    next_days = {}
+    all_days = sorted(days.keys())
+    for i, day in enumerate(all_days):
+        previous_days[day] = all_days[i-1] if i>0 else None
+        next_days[day] = all_days[i+1] if i<len(all_days)-1 else None
     
     # Go through all days and create a separate file for each, but only if one 
     # of the entries has been modified more recently than the file
     for day in sorted(days.keys()):
-        file= join(out, '{}.html'.format(day))
+        file = join(out, '{}.html'.format(day))
         if not exists(file) or any(entry.mtime>getmtime(file) for entry in days[day]):
             with open(file, 'w') as f:
-                f.write(template.render(entries=days[day]))
+                f.write(template.render(entries=days[day], 
+                    next_day=next_days[day], previous_day=previous_days[day]))
                 #TODO log this intstead of printing it
                 #TODO allow specifying log detail level from cli
                 print('Writing to:', file)
+
+    # Finally copy across stylesheet
+    stylesheet_template = env.get_template('style.css')
+    stylesheet_file = join(out, 'style.css')
+    if not exists(stylesheet_file):
+        with open(stylesheet_file, 'w') as f:
+            f.write(stylesheet_template.render())
+            print('Writing to: style.css')
