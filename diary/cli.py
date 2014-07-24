ba2bbd4d3d3131d0ce7d3296f5e0fc6a2471fda0
David Moody <david@davidxmoody.com>
2014-07-24 14:48:56 +0100

    Added --clean option to generate script

--- diary/cli.py
+++ diary/cli.py
@@ -200,6 +200,8 @@ subparser.add_argument('-o', '--out',
     help='directory to place HTML (defaults to {your_base_dir}/html)')
 subparser.add_argument('-w', '--watch', action='store_true',
     help='stay alive and update the HTML whenever entries change')
+subparser.add_argument('-c', '--clean', action='store_true',
+    help='remove out dir before generating')
 
 
 subparser.set_defaults(func=generate_command)
--- diary/generator.py
+++ diary/generator.py
@@ -1,14 +1,21 @@
 from jinja2 import Environment, PackageLoader
-import os
+from os.path import join, realpath, expanduser, expandvars, exists, getmtime
+from os import makedirs
+from shutil import rmtree
 
-def generate_command(conn, out, watch, **kwargs):
+def generate_command(conn, out, watch, clean, **kwargs):
 
     # Check output directory exists, if not then create it
     if not out:
-        out = os.path.join(conn.dir_base, 'html')
-    out = os.path.realpath(os.path.expanduser(os.path.expandvars(out)))
-    if not os.path.exists(out):
-        os.makedirs(out)
+        out = join(conn.dir_base, 'html')
+    out = realpath(expanduser(expandvars(out)))
+    if not exists(out):
+        makedirs(out)
+
+    # Clean out dir
+    if clean:
+        rmtree(out)
+        makedirs(out)
 
     # Load templates
     env = Environment(loader=PackageLoader('diary', 'templates'), 
@@ -27,11 +34,10 @@ def generate_command(conn, out, watch, **kwargs):
     # Go through all days and create a separate file for each, but only if one 
     # of the entries has been modified more recently than the file
     for day in sorted(days.keys()):
-        filename = os.path.join(out, '{}.html'.format(day))
-        file_mtime = os.path.getmtime(filename)
-        if any(entry.mtime>file_mtime for entry in days[day]):
-            with open(filename, 'w') as f:
+        file= join(out, '{}.html'.format(day))
+        if not exists(file) or any(entry.mtime>getmtime(file) for entry in days[day]):
+            with open(file, 'w') as f:
                 f.write(template.render(entries=days[day]))
                 #TODO log this intstead of printing it
                 #TODO allow specifying log detail level from cli
-                print('Writing to:', filename)
+                print('Writing to:', file)
