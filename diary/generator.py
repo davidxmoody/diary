8ba47cc12d6b8aa1c5a202c95e5764efef165df0
David Moody <david@davidxmoody.com>
2014-07-24 14:26:18 +0100

    Generator script actually generates entries

--- diary/generator.py
+++ diary/generator.py
@@ -1,11 +1,37 @@
 from jinja2 import Environment, PackageLoader
+import os
 
 def generate_command(conn, out, watch, **kwargs):
+
+    # Check output directory exists, if not then create it
+    if not out:
+        out = os.path.join(conn.dir_base, 'html')
+    out = os.path.realpath(os.path.expanduser(os.path.expandvars(out)))
+    if not os.path.exists(out):
+        os.makedirs(out)
+
+    # Load templates
     env = Environment(loader=PackageLoader('diary', 'templates'), 
             trim_blocks=True, lstrip_blocks=True)
 
     template = env.get_template('entries.html')
 
-    entries = conn.get_entries()
-
-    print(template.render(entries=entries))
+    # Build dict mapping days to entries occurring on that day
+    days = {}
+    for entry in conn.get_entries():
+        day = entry.date.strftime('%F')
+        if day not in days:
+            days[day] = []
+        days[day].append(entry)
+    
+    # Go through all days and create a separate file for each, but only if one 
+    # of the entries has been modified more recently than the file
+    for day in sorted(days.keys()):
+        filename = os.path.join(out, '{}.html'.format(day))
+        file_mtime = os.path.getmtime(filename)
+        if any(entry.mtime>file_mtime for entry in days[day]):
+            with open(filename, 'w') as f:
+                f.write(template.render(entries=days[day]))
+                #TODO log this intstead of printing it
+                #TODO allow specifying log detail level from cli
+                print('Writing to:', filename)
